eab31966ffa79e024aa03859dba11832f3396977
Leoc O'Cear <paincompiler@LeocdeMac.local>
2014-07-08 07:02:27 -0700

    Initial Commit

---
+++ login/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  login
+//
+//  Created by Leoc O'Cear on 14-7-8.
+//  Copyright (c) 2014年 Leoc O'Cear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ login/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  login
+//
+//  Created by Leoc O'Cear on 14-7-8.
+//  Copyright (c) 2014年 Leoc O'Cear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
